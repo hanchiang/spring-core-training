35d7cb84f4466afb7742bf18599aca868a309bdc
Han Chiang <yaphc@hotmail.com>
2021-10-12 18:26:26 +0800

    Annotation-driven configuration, in place of bean configuration

--- lab/16-annotations/src/main/java/config/RewardsConfig.java
+++ lab/16-annotations/src/main/java/config/RewardsConfig.java
@@ -1,16 +1,8 @@
 package config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import rewards.RewardNetwork;
-import rewards.internal.RewardNetworkImpl;
-import rewards.internal.account.AccountRepository;
-import rewards.internal.account.JdbcAccountRepository;
-import rewards.internal.restaurant.JdbcRestaurantRepository;
-import rewards.internal.restaurant.RestaurantRepository;
-import rewards.internal.reward.JdbcRewardRepository;
-import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
 
@@ -21,6 +13,7 @@ import javax.sql.DataSource;
  * - Save all changes, Re-run the RewardNetworkTests.  It should now pass.
  */
 @Configuration
+@ComponentScan("rewards.internal")
 public class RewardsConfig {
 
 	DataSource dataSource;
@@ -28,34 +21,6 @@ public class RewardsConfig {
 	@Autowired
 	public RewardsConfig(DataSource dataSource) {
 		this.dataSource = dataSource;
-	}
-		
-	@Bean
-	public RewardNetwork rewardNetwork(){
-		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
-	}
-	
-	@Bean
-	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository();
-		repository.setDataSource(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
-	}
-	
-	@Bean
-	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository();
-		repository.setDataSource(dataSource);
-		return repository;
 	}
 	
 	// TODO-02: Remove all of the @Bean methods above.
--- lab/16-annotations/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
+++ lab/16-annotations/src/main/java/rewards/internal/restaurant/JdbcRestaurantRepository.java
@@ -1,7 +1,9 @@
 package rewards.internal.restaurant;
 
 import common.money.Percentage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -42,6 +44,7 @@ import java.util.Map;
  *   We will fix this error in the next step.
  */
 
+@Repository
 public class JdbcRestaurantRepository implements RestaurantRepository {
 
 	private DataSource dataSource;
@@ -58,6 +61,7 @@ public class JdbcRestaurantRepository implements RestaurantRepository {
 	 * Restaurant cache is populated for read only access
 	 */
 
+	@Autowired
 	public JdbcRestaurantRepository(DataSource dataSource) {
 		this.dataSource = dataSource;
 		this.populateRestaurantCache();
